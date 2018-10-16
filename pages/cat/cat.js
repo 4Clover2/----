1ae1f9b3586c1a35891e4d40997f0551d477d0ed
4Clover2 <39738415+4Clover2@users.noreply.github.com>
2018-10-16 08:40:11 +0800

    Add files via upload

---
+++ pages/cat/cat.js
@@ -0,0 +1,15 @@
+Page({
+  data:{
+    related:[
+      {
+        title:'护肝吃它，补肾吃它，抗衰老吃它，从头补到脚，一吃一个准',
+        pic:'/images/tomato.jpg'
+      },
+      {
+        title:'珍贵合影，看完心情大好！',
+        pic:'/images/scene.jpg'
+      }
+    ],
+    count: 0
+  }
+})
